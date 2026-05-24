93e9d94db953586fbe8ac35a249f9ada64138d17
Tiger <holygawdjesus@gmail.com>
2026-05-24 03:20:25 -0700

    flow

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Results } from './pages/Results';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
