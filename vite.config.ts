8ac43b1f6ca54b1dbd988c1a650fb233b5bd0533
Shulin Lu <99631231+HOLYGAWDJESUS@users.noreply.github.com>
2026-05-25 13:13:08 -0700

    Update Vite config to set base path directly
    Simplify base path configuration in Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
-
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const isUserPage = repoName?.endsWith('.github.io');
-const base = process.env.BASE_PATH ?? (process.env.GITHUB_ACTIONS && repoName && !isUserPage ? `/${repoName}/` : '/');
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base,
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
